cf7370ea20762adbf3c290537d0d5a7265e599d6
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-20 10:09:43 +0200

    server.js copied from last projekt..

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { siteData } from "./src/models.js";
+import { siteView } from "./src/views.js";
+import path from "path";
+
+const __dirname = path.resolve(path.dirname(""));
+
+const app = express();
+const port = process.env.PORT || 3007;
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./public/views"));
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+// app.get('/', (req, res) => {
+//     res.send(siteView(siteData));
+// });
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
