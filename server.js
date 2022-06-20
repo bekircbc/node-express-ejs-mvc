80f7dc7d1f502af25d25524d840ca140aafba15b
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-20 11:50:38 +0200

    currnetpage added

--- server.js
+++ server.js
@@ -8,34 +8,66 @@ const __dirname = path.resolve(path.dirname(""));
 const app = express();
 const port = process.env.PORT || 3007;
 
+let currentPage = "";
+
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./src/views"));
 
 app.get("/", (req, res) => {
+  siteData.currentPage = "welcome";
   res.render("pages/index", siteData);
 });
 
 app.get("/nouns", (req, res) => {
+  siteData.currentPage = "nouns";
   res.render("pages/nouns", siteData);
 });
 
 app.get("/books", (req, res) => {
+  siteData.currentPage = "books";
   res.render("pages/books", siteData);
 });
 
 app.get("/translations", (req, res) => {
+  siteData.currentPage = "translations";
   res.render("pages/translations", siteData);
 });
 
 app.get("/landscape-photos", (req, res) => {
+  siteData.currentPage = "landscape-photos";
   res.render("pages/landscape-photos", siteData);
 });
 
 app.get("/jobs", (req, res) => {
+  siteData.currentPage = "jobs";
   res.render("pages/jobs", siteData);
 });
+
+// app.get("/", (req, res) => {
+//   res.render("pages/index", siteData);
+// });
+
+// app.get("/nouns", (req, res) => {
+//   res.render("pages/nouns", siteData);
+// });
+
+// app.get("/books", (req, res) => {
+//   res.render("pages/books", siteData);
+// });
+
+// app.get("/translations", (req, res) => {
+//   res.render("pages/translations", siteData);
+// });
+
+// app.get("/landscape-photos", (req, res) => {
+//   res.render("pages/landscape-photos", siteData);
+// });
+
+// app.get("/jobs", (req, res) => {
+//   res.render("pages/jobs", siteData);
+// });
 
 // app.get("/", (req, res) => {
 //   res.send(siteView(siteData));
